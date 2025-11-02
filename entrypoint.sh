d468357ec81e3eab7c8396056bca3da703024b1b
morey-tech <nicholas@morey.tech>
2025-11-02 16:32:19 -0500

    feat: improve log messages during set up

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-
 set -e
 
+echo ''
+echo 'Configuring Bitwarden CLI server URL'
+echo ''
 bw config server ${BW_HOST}
 
+echo ''
+echo 'Logging into Bitwarden CLI'
+echo ''
 bw login --apikey
 export BW_SESSION=$(bw unlock ${BW_PASSWORD} --raw)
+
+echo ''
+echo 'Validating Bitwarden unlock'
+echo ''
 bw unlock --check
 
 echo ''
 echo 'Running `bw server` on port 8087'
 echo ''
-
 bw serve --hostname 0.0.0.0 #--disable-origin-protection
